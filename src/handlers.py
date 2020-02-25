a88b56641886d804828ac0e8057ba975819a3c1d
Adrien Ferrand <ferrand.ad@gmail.com>
2020-02-25 21:42:44 +0100

    Go to the simplest form

--- src/handlers.py
+++ src/handlers.py
@@ -1,66 +1,54 @@
-import asyncio
-from typing import Dict
 import os
-import logging
 
-import kopf
-from kubernetes import config, client
+from kubernetes import config, client, watch
 
-TASKS: Dict[str, asyncio.Task] = {}
 VELERO_ANNOTATION = 'backup.velero.io/backup-volumes'
 
 
-@kopf.on.resume('', 'v1', 'pods')
-@kopf.on.create('', 'v1', 'pods')
-@kopf.on.update('', 'v1', 'pods')
-async def handle_pod(namespace, name, body, logger, **_):
-    if namespace.startswith('kube-'):
-        return
+def main():
+    if os.path.exists('/var/run/secrets/kubernetes.io/serviceaccount/token'):
+        config.load_incluster_config()
+    else:
+        config.load_kube_config()
+    v1 = client.CoreV1Api()
 
-    key = '{}/{}'.format(namespace, name)
-    if key in TASKS:
-        TASKS[key].cancel()
+    print('Starting controller.')
 
-    TASKS[key] = asyncio.create_task(reconcile_pod_annotations(namespace, name, body, logger))
-
-
-@kopf.on.delete('', 'v1', 'pods')
-async def remove_pod_watch(namespace, name, **_):
-    key = '{}/{}'.format(namespace, name)
-    if key in TASKS:
-        TASKS[key].cancel()
-
-
-async def reconcile_pod_annotations(namespace: str, name: str, body: Dict, logger: logging.Logger):
+    watcher = watch.Watch()
     try:
-        if os.path.exists('/var/run/secrets/kubernetes.io/serviceaccount/token'):
-            config.load_incluster_config()
-        else:
-            config.load_kube_config()
-        v1 = client.CoreV1Api()
+        for event in watcher.stream(v1.list_pod_for_all_namespaces):
+            pod = event['object']
+            name = pod.metadata.name
+            namespace = pod.metadata.namespace
+            if event['type'] not in ['ADDED', 'MODIFIED']:
+                continue
+            if pod.metadata.namespace.startswith('kube-'):
+                continue
+            if pod.status.phase != 'Running':
+                continue
 
-        if body['status']['phase'] != 'Running':
-            return
+            persistent_volumes = [volume.name for volume in pod.spec.volumes if volume.persistent_volume_claim]
+            if not persistent_volumes:
+                continue
 
-        persistent_volumes = [volume['name'] for volume in body['spec'].get('volumes', []) if volume.get('persistentVolumeClaim')]
+            annotations = pod.metadata.annotations if pod.metadata.annotations else {}
+            velero_annotation = annotations.get(VELERO_ANNOTATION)
+            velero_annotation = velero_annotation.split(',') if velero_annotation else []
 
-        annotations = body['metadata'].get('annotations', {})
-        velero_annotation = annotations.get(VELERO_ANNOTATION)
-        velero_annotation = velero_annotation.split(',') if velero_annotation else []
+            missing_volumes = list(set(persistent_volumes) - set(velero_annotation))
 
-        missing_volumes = list(set(persistent_volumes) - set(velero_annotation))
+            if missing_volumes:
+                print('Reconciling velero annotations on pod {}/{}: persistent_volumes={}, velero_annotation={}, missing={}'
+                      .format(namespace, name, persistent_volumes, velero_annotation, missing_volumes))
 
-        if missing_volumes:
-            logger.warning('Reconciling pod: persistent_volumes={}, velero_annotation={}, missing={}'
-                           .format(persistent_volumes, velero_annotation, missing_volumes))
+                velero_annotation.extend(missing_volumes)
+                annotations[VELERO_ANNOTATION] = ','.join(velero_annotation)
+                setattr(pod.metadata, 'annotations', annotations)
 
-            velero_annotation.extend(missing_volumes)
-            annotations[VELERO_ANNOTATION] = ','.join(velero_annotation)
+                v1.patch_namespaced_pod(name, namespace, pod)
+    except KeyboardInterrupt:
+        print('Closing controller.')
 
-            pod = v1.read_namespaced_pod(name, namespace)
-            setattr(pod.metadata, 'annotations', annotations)
 
-            v1.patch_namespaced_pod(name, namespace, pod)
-
-    except BaseException as error:
-        logger.error('Error occured while processing reconciliation for pod {}/{}: {}'.format(namespace, name, error))
+if __name__ == '__main__':
+    main()
--- src/setup.py
+++ src/setup.py
@@ -13,6 +13,6 @@ setuptools.setup(
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
-    python_requires='>=3.6',
+    python_requires='>=3.7',
     install_requires=['kopf==0.25', 'kubernetes==10.0.1'],
 )
